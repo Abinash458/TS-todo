ec618c0c63327d5f8df22a617e0cc71fd1309b27
Abinash Mohapatra <abinashmohapatra@Abinashs-MacBook-Pro.local>
2024-12-02 13:25:55 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import AddTodo from "./components/addtodo";
+import Navbar from "./components/navbar";
+import Todos from "./components/todos";
+import "./App.css";
+
+const App = () => {
+  return (
+    <main>
+      <h1>TODO REACT + TYPESCTIPT </h1>
+      <Navbar />
+      <AddTodo />
+      <Todos />
+    </main>
+  );
+};
+
+export default App;
